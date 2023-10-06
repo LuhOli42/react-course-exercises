25c60c31313c02b0d3745a678b92828fcf0b55f3
Ideraldo Silva <luh.oli.42@gmail.com>
2023-10-05 22:59:16 -0300

    sessao 5

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,31 +1,44 @@
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import React from "react";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = React.useState("2020");
+
+  const saveExpansesFilterHandler = (enteredExpansesFilter) => {
+    setFilteredYear(enteredExpansesFilter);
+  };
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.itens[0].title}
-        amount={props.itens[0].amount}
-        date={props.itens[0].date}
-      />
-      <ExpenseItem
-        title={props.itens[1].title}
-        amount={props.itens[1].amount}
-        date={props.itens[1].date}
-      />{" "}
-      <ExpenseItem
-        title={props.itens[2].title}
-        amount={props.itens[2].amount}
-        date={props.itens[2].date}
-      />{" "}
-      <ExpenseItem
-        title={props.itens[2].title}
-        amount={props.itens[2].amount}
-        date={props.itens[2].date}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onSaveExpansesFilter={saveExpansesFilterHandler}
+        />
+        <ExpenseItem
+          title={props.itens[0].title}
+          amount={props.itens[0].amount}
+          date={props.itens[0].date}
+        />
+        <ExpenseItem
+          title={props.itens[1].title}
+          amount={props.itens[1].amount}
+          date={props.itens[1].date}
+        />{" "}
+        <ExpenseItem
+          title={props.itens[2].title}
+          amount={props.itens[2].amount}
+          date={props.itens[2].date}
+        />{" "}
+        <ExpenseItem
+          title={props.itens[2].title}
+          amount={props.itens[2].amount}
+          date={props.itens[2].date}
+        />
+      </Card>
+    </div>
   );
 };
 
